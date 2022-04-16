79c48523a61d262d6a3821728e40c6712de87355
Tim Nibert <docz2a@gmail.com>
2022-04-16 22:04:57 +1000

    slightly more robust unit test

--- src/observer.rs
+++ src/observer.rs
@@ -7,10 +7,23 @@ use std::cell::RefCell;
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 enum Event {
     Test1,
-    // attach more info
     // todo: how to subscribe with the string being arbitrary?
     Test2(String)
 }
+
+/*
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Event {
+    name: String
+}
+
+impl Event {
+    fn new(name: String) -> Event {
+        Self {
+            name: name
+        }
+    }
+}*/
 
 trait Observer {
     // do we want to include a source?
@@ -71,36 +84,64 @@ impl Observable {
 mod tests {
     use super::*;
 
-    struct TestObserver {}
+    struct TestObserver {
+        t1_notifies: u32,
+        t2_notifies: u32
+    }
+
+    impl TestObserver {
+        fn new() -> TestObserver {
+            Self {
+                t1_notifies: 0,
+                t2_notifies: 0
+            }
+        }
+    }
 
     impl Observer for TestObserver {
         fn notify(&mut self, e: Event) {
             println!("Received {:?}", e);
+            match e {
+                Event::Test1 => self.t1_notifies = self.t1_notifies + 1,
+                Event::Test2(mystr) => self.t2_notifies = self.t2_notifies + 1
+            }
         }
     }
 
     #[test]
     fn test_notify() {
         // create observers
-        let testobserver1 = Rc::new(RefCell::new(TestObserver {}));
+        let testobserver1 = Rc::new(RefCell::new(TestObserver::new()));
+        // why can I not pass Rc::downgrade(&testobserver1) to subscribe()?
         let testdg1 = Rc::downgrade(&testobserver1);
-        let testobserver2 = Rc::new(RefCell::new(TestObserver {}));
-        let testdg2 = Rc::downgrade(&testobserver2);
-        let testobserver3 = Rc::new(RefCell::new(TestObserver {}));
-        let testdg3 = Rc::downgrade(&testobserver3);
-        let testobserver4 = Rc::new(RefCell::new(TestObserver {}));
-        let testdg4 = Rc::downgrade(&testobserver4);
+        let testdg2 = Rc::downgrade(&testobserver1);
+        let testobserver2 = Rc::new(RefCell::new(TestObserver::new()));
+        let testdg3 = Rc::downgrade(&testobserver2);
+        //let testobserver3 = Rc::new(RefCell::new(TestObserver::new()));
+        //let testdg3 = Rc::downgrade(&testobserver3);
+        //let testobserver4 = Rc::new(RefCell::new(TestObserver::new()));
+        //let testdg4 = Rc::downgrade(&testobserver4);
 
         // create observable and subscriptions
         let mut testobservable: Observable = Observable::new();
         testobservable.subscribe(Event::Test1, testdg1);
-        testobservable.subscribe(Event::Test1, testdg2);
+        testobservable.subscribe(Event::Test2("test".to_string()), testdg2);
         testobservable.subscribe(Event::Test1, testdg3);
-        testobservable.subscribe(Event::Test1, testdg4);
+        //testobservable.subscribe(Event::Test1, testdg3);
+        //testobservable.subscribe(Event::Test1, testdg4);
 
         // notify events
         testobservable.notify(Event::Test1);
-        // todo: make this one work with a variable string
+        assert_eq!(testobserver1.borrow().t1_notifies, 1);
+        assert_eq!(testobserver1.borrow().t2_notifies, 0);
+        assert_eq!(testobserver2.borrow().t1_notifies, 1);
+        assert_eq!(testobserver2.borrow().t2_notifies, 0);
+
+        // todo: make this one work with a variable string (see struct comments above)
         testobservable.notify(Event::Test2("test".to_string()));
+        assert_eq!(testobserver1.borrow().t1_notifies, 1);
+        assert_eq!(testobserver1.borrow().t2_notifies, 1);
+        assert_eq!(testobserver2.borrow().t1_notifies, 1);
+        assert_eq!(testobserver2.borrow().t2_notifies, 0);
     }
 }
