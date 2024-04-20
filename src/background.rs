3374eb3cb4d4e5ec7719d076c8de8cb41675eee6
Timothy Nibert <docz2a@gmail.com>
2024-04-20 22:04:34 +1000

    subscribe the game to map visibility changes

--- src/background.rs
+++ src/background.rs
@@ -1,5 +1,5 @@
 use crate::imgload::load_image_asset_buffer;
-use crate::observer::Listener;
+use crate::observer::{Listener, Observable};
 use crate::gameobject::GameObject;
 use crate::collision::Rect;
 use crate::game::{SCREEN_WIDTH, SCREEN_HEIGHT};
@@ -12,6 +12,7 @@ pub const STEP: i32 = 3;
 pub struct Background {
     full_image: im::RgbaImage,
     pub observer: Rc<Listener>,
+    pub signals_out: Observable,
     crop_corner_x: i32,
     crop_corner_y: i32
 }
@@ -21,8 +22,19 @@ impl Background {
         Self {
             full_image: load_image_asset_buffer(fname),
             observer: Rc::new(Listener::new()),
+            signals_out: Observable::new("background".to_string()),
             crop_corner_x: 0,
             crop_corner_y: 0
+        }
+    }
+
+    // return a rect of the current window in world coordinates
+    pub fn window(&self) -> Rect {
+        Rect {
+            x: self.crop_corner_x as f64,
+            y: self.crop_corner_y as f64,
+            w: SCREEN_WIDTH as f64,
+            h: SCREEN_HEIGHT as f64
         }
     }
 }
@@ -64,15 +76,19 @@ impl GameObject for Background {
             match e.as_str() {
                 "up" => {
                     self.crop_corner_y -= STEP;
+                    self.signals_out.notify("visibility_change".to_string())
                 },
                 "down" => {
                     self.crop_corner_y += STEP;
+                    self.signals_out.notify("visibility_change".to_string())
                 },
                 "left" => {
                     self.crop_corner_x -= STEP;
+                    self.signals_out.notify("visibility_change".to_string())
                 },
                 "right" => {
                     self.crop_corner_x += STEP;
+                    self.signals_out.notify("visibility_change".to_string())
                 },
                 _ => ()
             }
--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,13 @@
 use crate::background::Background;
 use crate::npc::NPC;
+use crate::observer::Listener;
 use crate::player::Player;
 use crate::input::Input;
 use crate::gameobject::GameObject;
 use crate::collision::Rect;
 use crate::sprite::Direction;
-use std::time::Instant;
+use std::rc::Rc;
+//use std::time::Instant;
 
 pub const SCREEN_WIDTH: u32 = 640;
 pub const SCREEN_HEIGHT: u32 = 480;
@@ -13,18 +15,21 @@ pub const FRAME_RATE: u32 = 45;
 
 pub struct Game {
     pub input: Input,
-    gameobjects: Vec<Box<dyn GameObject>>
+    gameobjects: Vec<Box<dyn GameObject>>,
+    observer: Rc<Listener>
 }
 
 impl Game {
     pub fn new() -> Self {
+        let game_observer = Rc::new(Listener::new());
+
         let player = Box::new(Player::new());
         let npc = Box::new(NPC::new(Direction::Right));
 
         //let mytilemap = Box::new(TileMap::new(2));
-        let bg = Box::new(Background::new("map.jpg"));
+        let mut bg = Box::new(Background::new("map.jpg"));
+        bg.signals_out.subscribe("visibility_change".to_string(), game_observer.clone());
 
-        // setup subscriptions
         let mut input = Input::new();
         input.subscribe(player.observer.clone(), vec!["up", "down", "left", "right"]);
         input.subscribe(bg.observer.clone(), vec!["up", "down", "left", "right"]);
@@ -32,7 +37,8 @@ impl Game {
 
         Game {
             input: input,
-            gameobjects: vec![bg, player, npc]
+            gameobjects: vec![bg, player, npc],
+            observer: game_observer
         }
     }
 }
@@ -47,7 +53,10 @@ impl GameObject for Game {
             if let Some(img) = g.render() {
                 if let Some(pos) = g.position() {
                     //let start = Instant::now();
+
+                    // todo: collision::convert_world_coord_to_screen_coord()
                     im::imageops::overlay(&mut screen_img, &img, pos.x as i64, pos.y as i64);
+
                     //let duration = start.elapsed();
                     //println!("overlay: {:?}", duration);
                 } else {
@@ -69,6 +78,16 @@ impl GameObject for Game {
     fn update(&mut self) {
         for u in self.gameobjects.iter_mut() {
             u.update();
+        }
+
+        for e in self.observer.poll_evt() {
+            match e.as_str() {
+                "visibility_change" => {
+                    // todo: track bounding rect
+                    println!("visibility");
+                },
+                _ => ()
+            }
         }
     }
 }
